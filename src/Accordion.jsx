274cf039599f5bbd2e26f91cfd628413b9a47eb7
sneha-gowda <snehagowda9899@gmail.com>
2022-01-09 19:03:07 +0530

    Assignment Only One Open copleted

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -2,22 +2,22 @@ import { React, useState } from "react";
 import "./Accordion.css"
 const Accordion = (Data) => {
     const contentOfCard = Data.Content;
-    const [active, setActiveTo] = useState(false)
-    const StateMonitor = () => {
-        setActiveTo((prevState) => {
-            if (prevState) {
-                return false
-            }
-            else {
-                return true;
-            }
-        })
-    }
+    // const [active, setActiveTo] = useState(false)
+    // const StateMonitor = () => {
+    //     setActiveTo((prevState) => {
+    //         if (prevState) {
+    //             return false
+    //         }
+    //         else {
+    //             return true;
+    //         }
+    //     })
+    // }
     return (
         <>
             <div className="accordion">
-                <button onClick={StateMonitor}>{Data.Name}</button>
-                <div className={`acco_cot ${ active ? "Active" : "notActive" } `}>
+                <button onClick={Data.onToggle}>{Data.Name}</button>
+                <div className={`accContent ${ Data.active ? "Active" : "notActive" } `}>
                     {contentOfCard.length > 0 && contentOfCard.map((ele,index) => {
                         return <h3 key={index}>{ele}</h3>
                     })}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react"
+import {React,useState} from "react"
 import Accordion from "./Accordion"
 import "./App.css"
 import Data from "./Data"
 const App = () => {
+  const [clicked, setClicked] = useState("0");
+  const handleToggle = (index) => {
+    if (clicked === index) {
+      return setClicked("0")
+    }
+    setClicked(index)
+  };
   const ArrayOfObjects = Data
   return (
     <>
       <div className="accordionContainer">
         {ArrayOfObjects.length > 0 && ArrayOfObjects.map((elem, index) => {
-          return <Accordion key={index} Name={elem.Name}  Content={elem.Content} />
+          return <Accordion onToggle={ ()=>handleToggle(index)}
+            active={clicked === index} key={index} Name={elem.Name}  Content={elem.Content} />
         })
         }
       </div>
