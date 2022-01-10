1e0b7fec9573f225f2b9a121605f31dabb45d505
sneha-gowda <snehagowda9899@gmail.com>
2022-01-10 15:36:20 +0530

    Assignment Open_Accordion_Components_to_right Completed

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,8 +1,7 @@
 const Data = [
-    { Name: "Topic1", Content: ["Content1", "Content2", "Content3"] }, 
-    { Name: "Topic2", Content: ["Content1", "Content2"] }, 
-    { Name: "Topic3", Content: ["Content1"] }, 
-    { Name: "Topic2", Content: ["Content1", "Content2"] }, 
-    { Name: "Topic3", Content: ["Content1"] }
+    { Name: "Topic1", Content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, doloremque nihil vel suscipit consequatur totam quis iure magnam odit harum atque unde cumque reiciendis ducimus, tenetur quisquam cum vitae reprehenderit quaerat debitis? Quo." }, 
+    { Name: "Topic2", Content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, doloremque nihil vel suscipit consequatur totam quis iure magnam odit harum atque unde cumque reiciendis ducimus, tenetur quisquam cum vitae reprehenderit quaerat debitis? Quo." }, 
+    { Name: "Topic3", Content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, doloremque nihil vel suscipit consequatur totam quis iure magnam odit harum atque unde cumque reiciendis ducimus, tenetur quisquam cum vitae reprehenderit quaerat debitis? Quo." }, 
+    
 ]
  export default Data
